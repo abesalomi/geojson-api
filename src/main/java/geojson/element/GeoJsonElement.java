8bfdb7bdd8c9868b6169a7e3e4033aed4dd392a3
Abesalomi Gogatishvili <abesalomi.gogatishvili@gmail.com>
2017-11-12 20:20:00 +0400

    Add JsonElement::toElasticsearchMap.

--- src/main/java/geojson/element/GeoJsonElement.java
+++ src/main/java/geojson/element/GeoJsonElement.java
@@ -1,9 +1,20 @@
 package geojson.element;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public interface GeoJsonElement<T> extends GeoJson {
 
     List<T> getCoordinates();
+
+    default Map<String, Object> toElasticsearchMap() {
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("type", getType().toLowerCase());
+        map.put("coordinates", getCoordinates());
+
+        return map;
+    }
 
 }
